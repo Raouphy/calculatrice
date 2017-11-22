91728eacfd52f464bebb3d2b42725ee7eadbd830
Raouphy <albertgnae@hotmail.fr>
2017-11-22 19:25:45 +0100

    calc

---
+++ main.js
@@ -0,0 +1,28 @@
+var inputLabel = document.getElementById('inputLabel');
+
+function pushBtn(obj) {
+
+    var pushed = obj.innerHTML;
+
+    if (pushed == '=') {
+        // Calculate
+        inputLabel.innerHTML = eval(inputLabel.innerHTML);
+
+    } else if (pushed == 'AC') {
+        // All Clear
+        inputLabel.innerHTML = '0';
+
+    }else if (pushed == 'CE') {
+
+      inputLabel.innerHTML = inputLabel.innerHTML.substring(0,inputLabel.innerHTML.length - 1)
+
+    } else {
+        if (inputLabel.innerHTML == '0') {
+            inputLabel.innerHTML = pushed;
+
+        } else {
+            inputLabel.innerHTML += pushed;
+
+        }
+    }
+}
